efe2d33dd2fa8bcaf0a559a7b9f85ca4fbb6865c
Razeenmia <rashaad.mia@gmial.com>
2024-10-02 19:19:02 +0200

    git push

---
+++ main.py
@@ -0,0 +1,12 @@
+def remove_h(string):
+  """Removes all occurrences of the letter 'H' from a string.
+
+  Args:
+    string: The input string.
+
+  Returns:
+    The string with all 'H' characters removed.
+  """
+
+  return string.replace('H', '')
+
